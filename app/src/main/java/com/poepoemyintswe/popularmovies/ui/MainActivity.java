b022d7518a174b486d41efcacd14c16ee74e4a99
Poe Poe <poepoe88@gmail.com>
2015-07-11 14:29:24 +0800

    proper app style

--- app/src/main/java/com/poepoemyintswe/popularmovies/ui/MainActivity.java
+++ app/src/main/java/com/poepoemyintswe/popularmovies/ui/MainActivity.java
@@ -1,16 +1,23 @@
 package com.poepoemyintswe.popularmovies.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.poepoemyintswe.popularmovies.R;
 
 public class MainActivity extends AppCompatActivity {
 
+  @Bind(R.id.toolbar) Toolbar toolbar;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+    setSupportActionBar(toolbar);
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
