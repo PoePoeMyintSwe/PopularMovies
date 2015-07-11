82ed31ea7f26ac24b8ca0d28a08f3b905b8e2279
Poe Poe <poepoe88@gmail.com>
2015-07-11 12:54:32 +0800

    photo url

--- app/src/main/java/com/poepoemyintswe/popularmovies/Config.java
+++ app/src/main/java/com/poepoemyintswe/popularmovies/Config.java
@@ -6,6 +6,7 @@ package com.poepoemyintswe.popularmovies;
 public class Config {
   public static final String BASE_URL = "http://api.themoviedb.org/3";
   public static final String DISCOVER_MOVIE = "/discover/movie";
+  public static final String PHOTO_URL = "http://image.tmdb.org/t/p/w185//";
   public static final String SORT_BY = "sort_by";
   public static final String API_KEY = "api_key";
 }
