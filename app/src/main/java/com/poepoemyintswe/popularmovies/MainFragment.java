c062a2b5f00af00cbf8a0e7603a36ecfa0d48ac2
Poe Poe <poepoe88@gmail.com>
2015-07-11 12:34:32 +0800

    api call
    added internet permission and successfully send api request

--- app/src/main/java/com/poepoemyintswe/popularmovies/MainFragment.java
+++ app/src/main/java/com/poepoemyintswe/popularmovies/MainFragment.java
@@ -1,7 +1,7 @@
 package com.poepoemyintswe.popularmovies;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +12,12 @@ import android.view.ViewGroup;
 public class MainFragment extends Fragment {
 
   public MainFragment() {
+  }
+
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    MyRestAdapter.getInstance().getMovies().subscribe();
   }
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
