3b3b5781f6bf35eb6c3520f261ba5bcafe0659dd
Poe Poe <poepoe88@gmail.com>
2015-07-12 05:24:15 +0800

    test

--- app/src/main/java/com/poepoemyintswe/popularmovies/ui/MovieDetailFragment.java
+++ app/src/main/java/com/poepoemyintswe/popularmovies/ui/MovieDetailFragment.java
@@ -1,18 +1,15 @@
 package com.poepoemyintswe.popularmovies.ui;
 
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Spannable;
-import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import butterknife.Bind;
@@ -30,28 +27,20 @@ import static com.poepoemyintswe.popularmovies.Config.PHOTO_URL;
 /**
  * Created by poepoe on 11/7/15.
  */
-public class MovieDetailFragment extends Fragment
-    implements ObservableScrollView.ScrollViewListener {
+public class MovieDetailFragment extends Fragment {
 
   @Bind(R.id.iv_backdrop) ImageView mBackDrop;
   @Bind(R.id.iv_movie_image) ImageView mPoster;
-  @Bind(R.id.fading_toolbar) Toolbar toolbar;
   @Bind(R.id.tv_overview) TextView mTvOverview;
   @Bind(R.id.tv_release_date) TextView mTvReleaseDate;
-  @Bind(R.id.tv_title) TextView mTvTitle;
+  @Bind(R.id.content) CoordinatorLayout coordinatorLayout;
+  @Bind(R.id.collapsing_toolbar) CollapsingToolbarLayout collapsingToolbar;
+  //@Bind(R.id.tv_title) TextView mTvTitle;
   @Bind(R.id.tv_language) TextView mTvLanguage;
-  @Bind(R.id.sv_content) ObservableScrollView mScrollView;
-  @Bind(R.id.fl_backdrop_container) FrameLayout mBackdropFrame;
-  @Bind(R.id.fl_thumbnail_container) FrameLayout mThumbnailFrame;
+  @Bind(R.id.toolbar) Toolbar toolbar;
 
   private static final String RESULT = "result";
   private static final String HEIGHT = "height";
-
-  private AlphaForegroundColorSpan mAlphaForegroundColorSpan;
-  private SpannableString mSpannableString;
-
-  private Drawable mActionBarBackgroundDrawable;
-  private int mActionBarTitleColor;
 
   private AppCompatActivity mActivity;
   private Result result;
@@ -77,25 +66,19 @@ public class MovieDetailFragment extends Fragment
 
   @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    ViewGroup rootView =
-        (ViewGroup) inflater.inflate(R.layout.fragment_movie_detail, container, false);
+    ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_testing, container, false);
     ButterKnife.bind(this, rootView);
 
     //Actionbar
-    mActionBarBackgroundDrawable = toolbar.getBackground();
-
-    mActionBarTitleColor = getResources().getColor(R.color.white);
-    mSpannableString = new SpannableString(result.getOriginalTitle());
-    mAlphaForegroundColorSpan = new AlphaForegroundColorSpan(mActionBarTitleColor);
-
-
     mActivity.setSupportActionBar(toolbar);
     ActionBar mActionBar = mActivity.getSupportActionBar();
     mActionBar.setDisplayHomeAsUpEnabled(true);
     mActionBar.setTitle("");
 
+    coordinatorLayout.dispatchDependentViewsChanged(mPoster);
     //Text
-    mTvTitle.setText(result.getTitle());
+    collapsingToolbar.setTitle(result.getTitle());
+    //mTvTitle.setText(result.getTitle());
     mTvOverview.setText(result.getOverview());
     mTvLanguage.setText(result.getOriginalLanguage());
     try {
@@ -108,6 +91,12 @@ public class MovieDetailFragment extends Fragment
     ViewGroup.LayoutParams params = mBackDrop.getLayoutParams();
     params.height = height;
     mBackDrop.setLayoutParams(params);
+
+    int newHeight = (height / 3) * 2;
+    ViewGroup.LayoutParams paramsPoster = mPoster.getLayoutParams();
+    paramsPoster.width = (newHeight / 3) * 2;
+    paramsPoster.height = newHeight;
+    mPoster.setLayoutParams(paramsPoster);
 
     Glide.with(getActivity())
         .load(BACKDROP_URL + result.getBackdropPath())
@@ -122,8 +111,6 @@ public class MovieDetailFragment extends Fragment
         .crossFade()
         .into(mPoster);
 
-    mScrollView.setScrollViewListener(this);
-
     return rootView;
   }
 
@@ -132,37 +119,4 @@ public class MovieDetailFragment extends Fragment
     SimpleDateFormat outputFormat = new SimpleDateFormat("MMM dd, yyyy");
     return outputFormat.format(inputFormat.parse(result.getReleaseDate()));
   }
-
-  @Override public void onScrollChanged(ObservableScrollView scrollView, int y, int oldy) {
-    int headerHeight = mBackdropFrame.getHeight() - toolbar.getHeight();
-    float ratio = 0;
-    if (oldy > 0 && headerHeight > 0) {
-      ratio = (float) Math.min(Math.max(oldy, 0), headerHeight) / headerHeight;
-    }
-    mBackdropFrame.setTop((int) ratio < 0 ? 0 : (int) ratio);
-    updateActionBarTransparency(ratio);
-    setTitleAlpha(clamp(5.0F * ratio - 4.0F, 0.0F, 1.0F));
-  }
-
-  private void updateActionBarTransparency(float scrollRatio) {
-    int newAlpha = (int) (scrollRatio * 255);
-    mActionBarBackgroundDrawable.setAlpha(newAlpha);
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-      toolbar.setBackground(mActionBarBackgroundDrawable);
-    } else {
-      toolbar.setBackgroundDrawable(mActionBarBackgroundDrawable);
-    }
-  }
-
-  private void setTitleAlpha(float alpha) {
-    mAlphaForegroundColorSpan.setAlpha(alpha);
-    mSpannableString.setSpan(mAlphaForegroundColorSpan, 0, mSpannableString.length(),
-        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-    toolbar.setTitle(mSpannableString);
-  }
-
-  public static float clamp(float value, float min, float max) {
-    return Math.max(min, Math.min(value, max));
-  }
-
 }
